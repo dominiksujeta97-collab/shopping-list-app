32c820e63f67f225ea083fe9085cbd9ec6ac323f
dominiksujeta97-collab <dominik.sujeta97@gmail.com>
2026-05-02 17:45:15 +0200

    Add files via upload

--- server/helpers/error.js
+++ server/helpers/error.js
@@ -1,4 +1,5 @@
 // Create validation error
+// Used when dtoIn does not match AJV validation schema
 function createValidationError(validationError) {
   return {
     code: "dtoInIsNotValid",
@@ -7,7 +8,9 @@ function createValidationError(validationError) {
   };
 }
 
+
 // Create not found error
+// Used for missing product or shopping list records
 function createNotFoundError(code, message) {
   return {
     code: code,
@@ -15,19 +18,34 @@ function createNotFoundError(code, message) {
   };
 }
 
+
 // Send error response
+// Maps application error codes to proper HTTP status codes
 function sendErrorResponse(res, error) {
-  if (error.code === "productNotFound") {
+  if (error.code === "dtoInIsNotValid") {
+    return res.status(400).json(error);
+  }
+
+  if (
+    error.code === "productAlreadyInShoppingList" ||
+    error.code === "productIsUsedInShoppingList"
+  ) {
+    return res.status(400).json(error);
+  }
+
+  if (error.code === "productNotInShoppingList") {
     return res.status(404).json(error);
   }
 
-  if (error.code === "dtoInIsNotValid") {
-    return res.status(400).json(error);
+  if (error.code === "productNotFound" || error.code === "shoppingListNotFound") {
+    return res.status(404).json(error);
   }
 
   return res.status(500).json(error);
 }
 
+
+// Export helper functions
 module.exports = {
   createValidationError,
   createNotFoundError,
