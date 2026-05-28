085f50d9e9659894dd61cc605b0a219628e34bd6
dominiksujeta97-collab <dominik.sujeta97@gmail.com>
2026-05-28 17:55:09 +0200

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 
 const productController = require("./controller/controller-product");
+const shoppingListController = require("./controller/controller-shoppingList");
 
 const app = express();
 const PORT = 3000;
+
+// CORS
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Accept"],
+  })
+);
 
 // Middleware
 app.use(express.json());
 
 // Controllers
 app.use("/products", productController);
+app.use("/shopping-lists", shoppingListController);
 
 // Test route
 app.get("/", (req, res) => {
